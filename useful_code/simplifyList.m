0a5cf4b4f22882c07bfc07247a8b307db330d6de
LiHuan <1981hli@gmail.com>
2018-11-19 20:37:54 +0800

    Update simplifyList.m

--- useful_code/simplifyList.m
+++ useful_code/simplifyList.m
@@ -1,1 +1,10 @@
-simplifyList[simplify_][list_] := Module[{count}, count = 1; apply[expr_] := Module[{time, temp}, time = SessionTime[]; temp = simplify[expr]; Print[{count++, SessionTime[] - time}]; temp]; Parallelize[Map[apply, list, {Length[Dimensions[list]]}]]]; 
+simplifyList[simplify_][list_] := Module[{count}, 
+    count = 1; 
+    apply[expr_] := Module[{time, temp}, 
+        time = SessionTime[]; 
+        temp = simplify[expr]; 
+        Print[{count++, SessionTime[] - time}]; 
+        temp
+    ]; 
+    Parallelize[Map[apply, list, {Length[Dimensions[list]]}]]
+]; 
